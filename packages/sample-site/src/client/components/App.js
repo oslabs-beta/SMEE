9c9afb6fceb2f8fa126de59f821dbc7382ed01b2
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2019-12-23 20:42:28 -0500

    added test package to sample site

--- packages/sample-site/src/client/components/App.js
+++ packages/sample-site/src/client/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import math from '@smee/exported';
 
 export default function App(props) {
   console.log(props);
+  const four = math.add(2, 2);
   // eslint-disable-next-line react/prop-types
   const { name } = props;
   console.log(name, typeof name);
-  return <h1>Hello {name}, from React func comp</h1>;
+  return (
+    <div>
+      <h1>Hello I'm {name}.</h1>
+      <img
+        style={{ height: '200px', width: '160px' }}
+        src="https://www.coleka.com/media/item/201702/22/pop-disney-disney-treasures-mr-smee.jpg"
+        alt="Mr. Smee"
+      />
+      <h2>I'm using a React function component.</h2>
+      <p>2 + 2 is {four}</p>
+    </div>
+  );
 }
--- packages/sample-site/src/client/index.js
+++ packages/sample-site/src/client/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 // eslint-disable-next-line no-undef
-ReactDOM.render(<App name="Rodrigo" />, document.getElementById('app'));
+ReactDOM.render(<App name="Smee" />, document.getElementById('app'));
