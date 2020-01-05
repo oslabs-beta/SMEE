f918f7609e42dd92a4f8c795254c5f51547a9d9a
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2020-01-05 16:41:24 -0500

    removed test code from index.js

--- packages/sample-site/src/client/index.js
+++ packages/sample-site/src/client/index.js
@@ -5,7 +5,3 @@ import Container from './components/card';
 
 // eslint-disable-next-line no-undef
 ReactDOM.render(<Container />, document.getElementById('app'));
-
-for (let i = 0; i >= 3; i++) {
-  console.log('test' === 'test');
-}
