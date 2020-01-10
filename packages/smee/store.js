9050352558fdebaa17a807934afd736c0ba75e1e
EggsBenedict2123 <EggsBenedict2123@users.noreply.github.com>
2020-01-10 14:05:03 -0500

    edited comments

--- packages/smee/store.js
+++ packages/smee/store.js
@@ -1,11 +1,10 @@
 // The store generator function, returns anonymous object through closure
 function generateStore() {
-  // generate our store object.
   const globalStore = {};
 
   // This get just grabs the data we hold in the piece of state currently being targeted and nothing else.
   const getter = state => ({
-    get: () => state.data
+    get: () => state.data,
   });
 
   // This sets initial state data or modifies our state data and updates state listeners.
@@ -13,7 +12,7 @@ function generateStore() {
     set: newData => {
       state.data = newData;
       state.listeners.forEach(x => x(newData));
-    }
+    },
   });
 
   // This will subscribe a component to the target piece of state, and return a function that will unsubscribe when invoked (useEffect).
@@ -22,38 +21,34 @@ function generateStore() {
       state.listeners.push(listener);
       return () =>
         (state.listeners = state.listeners.filter(x => x !== listener));
-    }
+    },
   });
 
   // We return an anonymous object that can set a piece of state or get a piece of state.
   return {
     setState: (name, data) => {
-      // Error handling
-      if (typeof name !== "string")
+      if (typeof name !== 'string')
         throw new Error("State name must be *type: 'String'*");
 
       globalStore[name] = (() => {
-        // Define our default empty state
         const state = { data: null, listeners: [] };
         // Using functional composition here-- This way keeps references to functions, allows code to be modified later if needed, and removes the extra syntax text on each build.
         return Object.assign(
           state,
           getter(state),
           setter(state),
-          subscriber(state)
+          subscriber(state),
         );
       })();
-      // Run a set on the new state to write data to object
       globalStore[name].set(data);
     },
     getState: name => {
-      // Error handling
-      if (typeof name !== "string")
+      if (typeof name !== 'string')
         throw new Error("State name must be *type: 'String'*");
 
-      // Return the store reference
+      // Return the store reference, not a copy
       return globalStore[name];
-    }
+    },
   };
 }
 // Export the Store object wrapped in closure for Smee functionality
--- packages/smee/useStore.js
+++ packages/smee/useStore.js
@@ -1,19 +1,16 @@
-// We import the react hooks we will use, and the Store we made in store.js.
-import { useState, useEffect } from "react";
-import { Store } from "./store";
+import { useState, useEffect } from 'react';
+import { Store } from './store';
 
-// The useStore hook takes in a name for the state and data the state holds.
+// The useStore hook
 export function useStore(stateName = null) {
-  // Error handling
   if (stateName === null)
     throw new Error("Parameter [state_name] of *type: 'String'* required");
-  if (typeof stateName !== "string")
+  if (typeof stateName !== 'string')
     throw new Error("State name must be *type: 'String'*");
 
   // Grab the piece of state in question from store
   const observable = Store.getState(stateName);
-  // Error handle in case it isn't found.
-  if (!observable) throw new Error("State requested not found in store");
+  if (!observable) throw new Error('State requested not found in store');
 
   // Run a useState hook to grab the data of state and a setter for it
   const [data, setData] = useState(observable.get());
@@ -21,6 +18,6 @@ export function useStore(stateName = null) {
   // Run a useEffect hook to link our Store to this component, and re-render on change
   useEffect(() => observable.subscribe(setData), [observable]);
 
-  // Return the data that updates on change, anywhere.
+  // Return the data that updates on the state objects' change
   return data;
 }
