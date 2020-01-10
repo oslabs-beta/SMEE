da4d3788c6f5113e1f6a4a884c205bae85ec6919
EggsBenedict2123 <EggsBenedict2123@users.noreply.github.com>
2020-01-10 15:59:51 -0500

    added dynamic state building functionality

--- packages/smee/useStore.js
+++ packages/smee/useStore.js
@@ -2,15 +2,23 @@ import { useState, useEffect } from 'react';
 import { Store } from './store';
 
 // The useStore hook
-export function useStore(stateName = null) {
+export function useStore(stateName = null, value = null) {
   if (stateName === null)
     throw new Error("Parameter [state_name] of *type: 'String'* required");
   if (typeof stateName !== 'string')
     throw new TypeError("State name must be *type: 'String'*");
 
   // Grab the piece of state in question from store
-  const observable = Store.getState(stateName);
-  if (!observable) throw new Error('State requested not found in store');
+  let observable = Store.getState(stateName);
+  // If no value is found, make new state and update the observer variable
+  if (!observable) {
+    if (value === null)
+      throw new Error(
+        `Attempted to make new state: ${stateName}, no value for ${stateName} in parameters`,
+      );
+    Store.setState(stateName, value);
+    observable = Store.getState(stateName);
+  }
 
   // Run a useState hook to grab the data of state and a setter for it
   const [data, setData] = useState(observable.get());
