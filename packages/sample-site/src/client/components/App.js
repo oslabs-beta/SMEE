e4f017a488a661fca94cadfc1917470295823aac
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2020-01-15 15:04:29 -0500

    sample site edits

--- packages/sample-site/src/client/components/App.js
+++ packages/sample-site/src/client/components/App.js
@@ -1,35 +1,41 @@
-import React, { useState } from 'react';
-import Button from './button';
+// import React, { useState } from 'react';
+// import Button from './button';
 
-export default function App(props) {
-  // eslint-disable-next-line react/prop-types
-  const { name } = props;
+// export default function App(props) {
+//   // eslint-disable-next-line react/prop-types
+//   const { name } = props;
 
-  // useState takes our initial state as a parameter
-  const [count, setCount] = useState(0);
-  const buttons = [];
+//   // useState takes our initial state as a parameter
+//   const [count, setCount] = useState(0);
+//   const buttons = [];
 
-  const handleCount = () => {
-    setCount(count + 1);
-  };
+//   const handleCount = () => {
+//     setCount(count + 1);
+//   };
 
-  for (let i = 0; i <= count; i += 1) {
-    buttons.push(<Button click={handleCount} key={i} />);
-  }
+//   for (let i = 0; i <= count; i += 1) {
+//     buttons.push(<Button click={handleCount} key={i} />);
+//   }
 
-  return (
-    <div>
-      <h1>Hello I&rsquo;m {name}.</h1>
-      <img
-        style={{ height: '200px', width: '160px' }}
-        src="https://www.coleka.com/media/item/201702/22/pop-disney-disney-treasures-mr-smee.jpg"
-        alt="Mr. Smee"
-      />
+//   return (
+//     <div>
+//       <h1>Hello I&rsquo;m {name}.</h1>
+//       <img
+//         style={{ height: '200px', width: '160px' }}
+//         src="https://www.coleka.com/media/item/201702/22/pop-disney-disney-treasures-mr-smee.jpg"
+//         alt="Mr. Smee"
+//       />
 
-      <div id="container-button">
-        <h3>{count}</h3>
-        {buttons}
-      </div>
-    </div>
-  );
+//       <div id="container-button">
+//         <h3>{count}</h3>
+//         {buttons}
+//       </div>
+//     </div>
+//   );
+// }
+
+import React from 'react';
+
+export default function App() {
+  return <h1>Hello world</h1>;
 }
--- packages/sample-site/src/client/index.js
+++ packages/sample-site/src/client/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import Container from './components/card';
+// import Container from './components/card';
 
 // ReactDOM.render(<Container />, document.getElementById('app'));
 ReactDOM.render(<App />, document.getElementById('app'));
